5f1706c94b45197d759186bed28c9310d046e617
cdomet-d <coralie.dometdemont@gmail.com>
2024-02-15 12:46:04 +0100

    added .gitignore

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:06:26 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/02/15 12:06:49 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/02/15 12:14:45 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,13 @@ int	main(int argc, char *argv[], char **envp)
 	printf("absolute path : %s\n", var.a_path);
 	ids.p1 = fork();
 	if (ids.p1 == 0)
+	{
 		execve(var.a_path, var.args, envp);
+		printf("This is the child process ! id = %d\n", ids.p1);
+	}
+	if (ids.p1 != 0)
+		wait(NULL);
 	printf("This should only be printed by the parent process, \
 		id = %d\n", ids.p1);
 	free_var(&var, EXIT_SUCCESS, NULL);
-	if (ids.p1 != 0)
-		wait(NULL);
 }
