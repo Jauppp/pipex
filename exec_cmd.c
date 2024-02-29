2563d827b69a4600bfc0ccac76028cc8004aff25
Jauppp <coralie.dometdemont@gmail.com>
2024-02-29 15:02:04 +0100

    trying

--- exec_cmd.c
+++ exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:03:59 by cdomet-d          #+#    #+#             */
-/*   Updated: 2024/02/29 14:46:07 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2024/02/29 15:01:46 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	exec_first_cmd(t_var *var)
 {
 	fprintf(stderr, "In first_cmd, %d\n", var->i);
+	var->tmpIn = var->fd1[R];
 	close(var->fd1[R]);
 	if (dup2(var->files[R], STDIN_FILENO) == -1)
 		free_var(var, errno, NULL);
@@ -33,6 +34,15 @@ void	exec_first_cmd(t_var *var)
 void	exec_cmd(t_var *var)
 {
 	fprintf(stderr, "In exec_cmd, %d\n", var->i);
+	if (dup2(var->tmpIn, var->fd1[R]) == -1)
+		free_var(var, errno, NULL);
+	if (dup2(var->fd1[R], STDIN_FILENO) == -1)
+		free_var(var, errno, NULL);
+	var->tmpIn = var->fd1[R];
+		close(var->fd1[R]);
+	if (dup2(var->fd1[W], STDOUT_FILENO) == -1)
+		free_var(var, errno, NULL);
+	close(var->fd1[W]);
 	var->id = fork();
 	if (var->id == -1)
 		free_var(var, errno, NULL);
@@ -44,6 +54,14 @@ void	exec_cmd(t_var *var)
 void	exec_last_cmd(t_var *var)
 {
 	fprintf(stderr, "In last_cmd, %d\n", var->i);
+	close(var->fd1[W]);
+	if (dup2(var->tmpIn, var->fd1[R]) == -1)
+		free_var(var, errno, NULL);
+	if (dup2(var->fd1[R], STDIN_FILENO) == -1)
+		free_var(var, errno, NULL);
+	close(var->fd1[R]);
+	// if (dup2(var->files[W], STDOUT_FILENO) == -1)
+	// 	free_var(var, errno, NULL);
 	var->id = fork();
 	if (var->id == -1)
 		free_var(var, errno, NULL);
